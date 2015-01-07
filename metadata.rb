36061edc9bdc87875140c49aed32b00fbe270876
cybermerc <nomad@cybermerc.org>
2015-01-06 20:35:31 -0600

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,14 @@ name              'patchman'
 maintainer        'Christopher Coffey'
 maintainer_email  'nomad@cybermerc.org'
 license           'Apache 2.0'
-description       'Setup OS patching configuration'
+description       'Linux OS patch management system'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.0.0'
+version           '1.0.1'
+
+supports 'ubuntu'
+supports 'centos'
+supports 'redhat'
+supports 'debian'
 
 depends 'yum'
 depends 'apt'
